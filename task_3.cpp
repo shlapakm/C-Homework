88a73e5af3fc8ca6e9608b5e5e37e92ce1522341
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2021-09-06 20:39:09 +0300

    Add files via upload

---
+++ task_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello! This program will help you solve quadratic equation! " << endl;
+    cout << "Please enter the coefficients a, b, c of your equation (ax^2 + bx + c = 0 ) separated by a space" << endl;
+    float a;
+    float b;
+    float c;
+    cin >> a >> b >> c;
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) cout << "No solution" << endl;
+    else
+    {
+        if (discriminant == 0) cout << "Solution is " << (-b) / (2 * a) << endl;
+        else
+        {
+            discriminant = sqrt(discriminant);
+            cout << "First solution is " << (-b + discriminant) / (2.0 * a) << endl;
+            cout << "Second solution is " << (-b - discriminant) / (2.0 * a) << endl;
+        }
+    }
+    
+
+    return 0;
+}
