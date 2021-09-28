28e78e8c90107b5329f0f2956410ba5b2c6085eb
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2021-09-28 11:43:17 +0300

    Update task_3.cpp

--- task_3.cpp
+++ task_3.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -5,25 +6,25 @@
 int main()
 {
     using namespace std;
-    vector <double> originalArray;
     cout << "Enter the number of array elements." << endl;
     int n;
     cin >> n;
+    vector <int> originalArray{ n };
     cout << "Enter the values of the array elements separated by a space." << endl;
     for (int i = 0; i < n; i++)
     {
         double value;
         cin >> value;
-        originalArray.push_back(value);
+        originalArray[i] = value;
     }
     sort(originalArray.begin(), originalArray.end(), [](double i, double j) {return (i < j); });
     cout << "Our sorted array in ascending order array: " << " ";
-    for (int i = 0; i < n; i++)
-        cout << originalArray[i] << " ";
+    for (auto i : originalArray)
+        cout << i << " ";
     cout << endl;
     sort(originalArray.begin(), originalArray.end(), [](double i, double j) {return (i > j); });
     cout << "Our sorted array in descending order array: " << " ";
-    for (int i = 0; i < n; i++)
-        cout << originalArray[i] << " ";
+    for (auto i : originalArray)
+        cout << i << " ";
     return 0;
 }
