0ce59cabc9058ffc4d7452dc1d2c35be92ca4d65
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2021-10-05 00:26:08 +0300

    Update task_3.cpp

--- task_3.cpp
+++ task_3.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 struct Time
 {
-    short day : 10; //since there are the same number of bits for negativeand positive values, we need 5 * 2 = 10
-    short month : 8;
-    short year;
-    short hours : 10;
-    short minutes : 12;
-    short seconds : 12;
-    short mseconds;
+    short day : 6; //since there are the same number of bits for negativeand positive values, we need 5 + 1 = 6 bites for 62 values
+    short month : 5;
+    short year : 12;
+    short hours : 6;
+    short minutes : 7;
+    short seconds : 7;
+    short mseconds : 11;
     void printTime()
     {
         std::cout << "Date and time of this event:" << std::endl;
