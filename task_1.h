f63202f497ee39a47bc941ef245721aac4351a75
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2021-10-26 19:53:21 +0300

    Add files via upload

--- task_1.h
+++ task_1.h
@@ -13,9 +13,15 @@ public:
 	{
 		std::cout << "Parents function" << std::endl;
 	}
-	virtual void getPerimetr()
+	virtual void getPerimeter()
 	{
 		std::cout << "Parents function" << std::endl;
+	}
+	virtual ~Figure() 
+	{
+		std::cout << "Calling ~Figure" << std::endl;
+		delete &_x;
+		delete &_y;
 	}
 };
 class Ellipse : public Figure
@@ -32,7 +38,7 @@ public:
 	{
 		std::cout << "Coordinates of our ellipse: (" << _x << ";" << _y << ") " << "a = " << _a << " " << "b = " << _b << std::endl;
 	}
-	virtual void getPerimetr()
+	virtual void getPerimeter()
 	{
 		const double Pi = 3.141592;
 		std::cout << "The perimetr of our ellipse is " << 4 * ((Pi * _a * _b + (_a - _b) * (_a - _b)) / (_a + _b)) << std::endl;
@@ -43,6 +49,30 @@ public:
 		_y = y;
 		_a = a;
 		_b = b;
+	}
+	double getXOfEllipse() const
+	{
+		return _x;
+	}
+	double getYOfEllipse() const
+	{
+		return _y;
+	}
+	double getAOfEllipse() const
+	{
+		return _a;
+	}
+	double getBOfEllipse() const
+	{
+		return _b;
+	}
+	virtual ~Ellipse() 
+	{
+		std::cout << "Calling ~Ellipse" << std::endl;
+		delete& _x;
+		delete& _y;
+		delete& _a;
+		delete& _b;
 	}
 };
 class Rectangle : public Figure
@@ -59,7 +89,7 @@ public:
 		const double Pi = 3.141592;
 		std::cout << "The area of our rectangle is " << _a * _b << std::endl;
 	}
-	virtual void getPerimetr()
+	virtual void getPerimeter()
 	{
 		std::cout << "The perimetr of our rectangle is " << 2 * (_a + _b) << std::endl;
 	}
@@ -69,6 +99,30 @@ public:
 		_y = y;
 		_a = a;
 		_b = b;
+	}
+	double getXORectangle() const
+	{
+		return _x;
+	}
+	double getYOfRectangle() const
+	{
+		return _y;
+	}
+	double getAOfRectangle() const
+	{
+		return _a;
+	}
+	double getBOfRectangle() const
+	{
+		return _b;
+	}
+	 virtual ~Rectangle()
+	{
+		std::cout << "Calling ~Rectangle" << std::endl;
+		delete& _x;
+		delete& _y;
+		delete& _a;
+		delete& _b;
 	}
 };
 class Triangle : public Figure
@@ -87,7 +141,7 @@ public:
 		S = sqrt((_a + _b + _c) * (-_a + _b + _c) * (_a - _b + _c) * (_a + _b - _c) / 16);
 		std::cout << "The area of our triangle is " << S << std::endl;
 	}
-	virtual void getPerimetr()
+	virtual void getPerimeter()
 	{
 		std::cout << "The perimetr of our triangle is " << _a + _b + _c << std::endl;
 	}
@@ -98,6 +152,35 @@ public:
 		_a = a;
 		_b = b;
 		_c = c;
+	}
+	double getXOfTriangle() const
+	{
+		return _x;
+	}
+	double getYOfTriangle() const
+	{
+		return _y;
+	}
+	double getAOfTriangle() const
+	{
+		return _a;
+	}
+	double getBOfTriangle() const
+	{
+		return _b;
+	}
+	double getCOfTriangle() const
+	{
+		return _c;
+	}
+	virtual ~Triangle()
+	{
+		std::cout << "Calling ~Triangle" << std::endl;
+		delete& _x;
+		delete& _y;
+		delete& _a;
+		delete& _b;
+		delete& _c;
 	}
 };
 class Circle : public Ellipse
@@ -110,6 +193,26 @@ public:
 		_a = a;
 		_b = a;
 	}
+	double getXOfCircle() const
+	{
+		return _x;
+	}
+	double getYOfCircle() const
+	{
+		return _y;
+	}
+	double getAOfCircle() const
+	{
+		return _a;
+	}
+	virtual ~Circle()
+	{
+		std::cout << "Calling ~Circle" << std::endl;
+		delete& _x;
+		delete& _y;
+		delete& _a;
+		delete& _b;
+	}
 };
 class Square : public Rectangle
 {
@@ -120,5 +223,25 @@ public:
 		_y = y;
 		_a = a;
 		_b = a;
+	}
+	double getXOfSquare() const
+	{
+		return _x;
+	}
+	double getYOfSquare() const
+	{
+		return _y;
+	}
+	double getAOfSquare() const
+	{
+		return _a;
+	}
+	virtual ~Square()
+	{
+		std::cout << "Calling ~Square" << std::endl;
+		delete& _x;
+		delete& _y;
+		delete& _a;
+		delete& _b;
 	}
 };
