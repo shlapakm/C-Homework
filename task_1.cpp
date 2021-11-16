372465ac14105f32e854b1648bf9a7c6d130094d
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2021-11-16 18:56:56 +0300

    Add files via upload

--- task_1.cpp
+++ task_1.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <cstring>
 //without convolution (with recursion)
 template <typename Arg>
 auto sizeFunction(Arg arg)
 {
-	return sizeof(arg);
+    return sizeof(arg);
+}
+template <>
+auto sizeFunction(const char* arg)
+{
+    return strlen(arg);
 }
 template <typename First, typename ... Args>
 auto sizeFunction(First first, Args... arg)
 {
-	return sizeof(first) + sizeFunction(arg...);
+    return sizeFunction(first) + sizeFunction(arg...);
 }
 //with convolution
 template <typename ... Args>
 auto sizeFunctionWithConvolution(Args... arg)
 {
-	return (... + sizeof(arg));
+    return (... + sizeFunction(arg));
 }
 int main()
 {
-	std::cout << sizeFunction(2, 4, 5, true, "Masha") << std::endl;
-	std::cout << sizeFunctionWithConvolution(2, 4, 5, true, "Masha");
-	return 0;
+    std::cout << sizeFunction(2, 4, 5, true, "M", false, 23.9) << std::endl;
+    std::cout << sizeFunctionWithConvolution(2, 4, 5, true, "M", false, 239.9, 'v') << std::endl;
+    return 0;
 }
